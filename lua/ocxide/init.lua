34aa265b82e5b1f42058935fe32e8e94733ff2c1
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-26 20:21:07 -0500

    feat: setup vim sets

--- lua/ocxide/init.lua
+++ lua/ocxide/init.lua
@@ -1,3 +1,5 @@
+require('ocxide.set')
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
---
+++ lua/ocxide/set.lua
@@ -0,0 +1,12 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Tab config
+vim.opt.expandtab = false
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.wrap = false
+
+-- clipboard
+vim.opt.clipboard = ""
