a0e7fcaa99a941b54f51f96685cb6211829b4431
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-27 13:14:36 -0500

    feat: add lsp completion

--- lua/ocxide/plugins/lsp.lua
+++ lua/ocxide/plugins/lsp.lua
@@ -3,31 +3,42 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig.nvim",
-		-- "hrsh7th/cmp-nvim-lsp",
-		-- "hrsh7th/cmp-buffer",
-		-- "hrsh7th/cmp-path",
-		-- "hrsh7th/cmp-cmdline",
-		-- "hrsh7th/nvim-cmp",
-		-- "L3MON4D3/LuaSnip",
-		-- "saadparwaiz1/cmp_luasnip",
+
+		-- Completion
+		"hrsh7th/nvim-cmp",
+
+		-- Sources
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		-- Completion engine
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
 		-- "j-hui/fidget.nvim",
 	},
 	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+
 		require("mason").setup()
 		require("mason-lspconfig").setup({
 			ensure_installed = { "lua_ls" },
 		})
 
 		local lspconfig = require("lspconfig")
-		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
 		local OcxideGroup = vim.api.nvim_create_augroup("ocxide", {})
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = OcxideGroup,
 			callback = function(event)
-				-- local client = vim.lsp.get_client_by_id(args.data.client_id)
-
 				require("ocxide-system.keymaps").load("lsp", { buffer = event.buf })
 			end,
 		})
@@ -36,6 +47,7 @@ return {
 			capabilities = capabilities,
 			settings = {
 				Lua = {
+					runtime = { version = "Lua 5.1" },
 					diagnostics = {
 						globals = { "vim" },
 					},
@@ -166,6 +178,33 @@ return {
 		lspconfig.astro.setup({
 			capabilities = capabilities,
 		})
+
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
+
+		cmp.setup {
+			snippet = {
+			expand = function(args)
+			 	require('luasnip').lsp_expand(args.body)
+			end,
+			},
+			mapping = cmp.mapping.preset.insert({
+					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				  ['<C-e>'] = cmp.mapping.abort(),
+					-- I don't know what this does
+					["<C-Space>"] = cmp.mapping.complete(),
+					-- TODO: Keep only one
+					['<C-l>'] = cmp.mapping.confirm({ select = true }),
+					['<C-}>'] = cmp.mapping.confirm({ select = true }),
+					['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+					{ name = 'nvim_lsp' },
+					{ name = 'luasnip' }, -- For luasnip users.
+			}, {
+					{ name = 'buffer' },
+			})
+		}
 
 		vim.diagnostic.config({
 			-- update_in_insert = true,
