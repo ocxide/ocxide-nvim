fb1efece60f595b63c8f3832c6332aeca6a64264
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-27 21:59:32 -0500

    feat: add lualine and dev-icons

--- lua/ocxide/plugins/file-browser.lua
+++ lua/ocxide/plugins/file-browser.lua
@@ -1,6 +1,6 @@
 return {
 	"nvim-telescope/telescope-file-browser.nvim",
-	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("telescope").load_extension("file_browser")
 	end,
