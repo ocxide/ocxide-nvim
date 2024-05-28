574b534d10a19e4638539d80de8208b90a5aa122
Jeshua Hinostroza <hinostrozajeshua@gmail.com>
2024-05-28 16:16:07 -0500

    fea: setup default lsp handler

--- lua/ocxide/plugins/lsp.lua
+++ lua/ocxide/plugins/lsp.lua
@@ -24,11 +24,6 @@ return {
 		local cmp = require("cmp")
 		local cmp_lsp = require("cmp_nvim_lsp")
 
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls" },
-		})
-
 		local lspconfig = require("lspconfig")
 		local capabilities = vim.tbl_deep_extend(
 			"force",
@@ -36,6 +31,17 @@ return {
 			vim.lsp.protocol.make_client_capabilities(),
 			cmp_lsp.default_capabilities()
 		)
+
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls" },
+			handlers = {
+				-- default handler
+				function(server_name)
+					require("lspconfig")[server_name].setup({ capabilities = capabilities })
+				end
+			},
+		})
 
 		local OcxideGroup = vim.api.nvim_create_augroup("ocxide", {})
 
@@ -187,27 +193,27 @@ return {
 
 		cmp.setup {
 			snippet = {
-			expand = function(args)
-			 	require('luasnip').lsp_expand(args.body)
-			end,
+				expand = function(args)
+					require('luasnip').lsp_expand(args.body)
+				end,
 			},
 			mapping = cmp.mapping.preset.insert({
-					['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-					['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-				  ['<C-e>'] = cmp.mapping.abort(),
-					-- I don't know what this does
-					["<C-Space>"] = cmp.mapping.complete(),
-					-- TODO: Keep only one
-					['<C-l>'] = cmp.mapping.confirm({ select = true }),
-					['<C-}>'] = cmp.mapping.confirm({ select = true }),
-					['<C-CR>'] = cmp.mapping.confirm({ select = true }),
+				['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+				['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+				['<C-e>'] = cmp.mapping.abort(),
+				-- I don't know what this does
+				["<C-Space>"] = cmp.mapping.complete(),
+				-- TODO: Keep only one
+				['<C-l>'] = cmp.mapping.confirm({ select = true }),
+				['<C-}>'] = cmp.mapping.confirm({ select = true }),
+				['<C-CR>'] = cmp.mapping.confirm({ select = true }),
 			}),
 			sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' }, -- For luasnip users.
-					{ name = 'nvim_lua' },
+				{ name = 'nvim_lsp' },
+				{ name = 'luasnip' }, -- For luasnip users.
+				{ name = 'nvim_lua' },
 			}, {
-					{ name = 'buffer' },
+				{ name = 'buffer' },
 			})
 		}
 
